278f20609439c5c5194aa1a813e08a8759f2e4a9
timManas <timothy.john.manas@gmail.com>
2015-06-28 06:11:30 -0400

    ChangeOver

--- app/src/main/java/experiment/illustro/parseloginauthentication/Utils/UserTouchFX.java
+++ app/src/main/java/experiment/illustro/parseloginauthentication/Utils/UserTouchFX.java
@@ -1,5 +1,8 @@
 package experiment.illustro.parseloginauthentication.Utils;
 
+import android.annotation.TargetApi;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -10,11 +13,23 @@ import android.view.View.OnTouchListener;
 public class UserTouchFX implements OnTouchListener
 {
 
+    private int ALPHA = 150;
 
-
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent)
     {
+
+        if(motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            Drawable drawable = view.getBackground();
+            drawable.mutate();
+            drawable.setAlpha(ALPHA);
+            view.setBackground(drawable);
+        }
+
+
+
         return false;
     }
 }
