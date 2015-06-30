b6ca6f3498e819b0d4d0d0e9899614032645e76b
timManas <timothy.john.manas@gmail.com>
2015-06-30 04:04:23 -0400

    ChangeOver

--- app/src/main/java/experiment/illustro/parseloginauthentication/custom/CustomActivity.java
+++ app/src/main/java/experiment/illustro/parseloginauthentication/custom/CustomActivity.java
@@ -1,8 +1,12 @@
 package experiment.illustro.parseloginauthentication.custom;
 
+import android.app.ActionBar;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+
+import experiment.illustro.parseloginauthentication.R;
+import experiment.illustro.parseloginauthentication.Utils.UserTouchFX;
 
 /**
  * Created by Spirit on 6/28/2015.
@@ -10,14 +14,31 @@ import android.view.View.OnClickListener;
 public class CustomActivity extends FragmentActivity implements OnClickListener
 {
 
+    public static final UserTouchFX TOUCH = new UserTouchFX();
+
     @Override
     public void setContentView(int layoutResID)
     {
         super.setContentView(layoutResID);
+        setupActionBar();
     }
 
     protected void setupActionBar()
     {
+        ActionBar actionBar = this.getActionBar();  // could also be just getActionBar instead of this.getActionBar
+
+        if(actionBar == null)
+        {
+            return;
+        }
+
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setBackgroundDrawable(this.getResources().getDrawable(R.drawable.blu_btn,null));
+        actionBar.setLogo(R.drawable.chatler_logo);
+
 
     }
 
